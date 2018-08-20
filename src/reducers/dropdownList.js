25f1f543ce79a126c7764518b5c925343309ca41
blackmumba <ok.semonenko@gmail.com>
2018-08-20 14:57:55 +0300

    move action names to constants

--- src/reducers/dropdownList.js
+++ src/reducers/dropdownList.js
@@ -1,3 +1,11 @@
+import {
+    SET_SELECTED_OPTION,
+    SET_ACTIVE_TRUE,
+    SET_ACTIVE_FALSE,
+    SET_UPWARD_TRUE,
+    SET_UPWARD_FALSE
+} from '../constants/DropdownList'
+
 const initialState = {
     selectedOption: null,
     active: false,
@@ -6,15 +14,15 @@ const initialState = {
 
 const dropdownList = (state = initialState, action) => {
     switch (action.type) {
-        case 'SET_SELECTED_OPTION':
+        case SET_SELECTED_OPTION:
             return {...state, selectedOption: action.payload};
-        case 'SET_ACTIVE_TRUE':
+        case SET_ACTIVE_TRUE:
             return {...state, active: true};
-        case 'SET_ACTIVE_FALSE':
+        case SET_ACTIVE_FALSE:
             return {...state, active: false};
-        case 'SET_UPWARD_TRUE':
+        case SET_UPWARD_TRUE:
             return {...state, upward: true};
-        case 'SET_UPWARD_FALSE':
+        case SET_UPWARD_FALSE:
             return {...state, upward: false};
 
 
--- src/reducers/searchBar.js
+++ src/reducers/searchBar.js
@@ -1,4 +1,9 @@
 import {PlaceholderPosition} from "../PlaceholderPosition";
+import {
+    SET_FILTER_TEXT,
+    SET_PLACEHOLDER,
+    SET_PLACEHOLDER_POSITION
+} from '../constants/SearchBar'
 
 const initialState = {
     filterText: '',
@@ -8,11 +13,11 @@ const initialState = {
 
 const searchBar = (state = initialState, action) => {
     switch (action.type) {
-        case 'SET_FILTER_TEXT':
+        case SET_FILTER_TEXT:
             return {...state, filterText: action.payload};
-        case 'SET_PLACEHOLDER':
+        case SET_PLACEHOLDER:
             return {...state, placeholder: action.payload};
-        case 'SET_PLACEHOLDER_POSITION':
+        case SET_PLACEHOLDER_POSITION:
             return {...state, placeholderPosition: action.payload};
 
         default:
