57f46d3d7723b95ad86aeeedfb10b46215e635d8
blackmumba <ok.semonenko@gmail.com>
2018-08-20 13:20:03 +0300

    remove connect from App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react'
-import {connect} from 'react-redux'
 
 import Dropdown from '../Dropdown/Dropdown'
 import styles from './App.module.styl'
 
-class App extends Component {
+export default class App extends Component {
     render() {
-        console.log('appState', this.props.appState);
         return (
             <div className={styles.app}>
                 <h1 className={styles['app__header']}>Dropdown list with search</h1>
@@ -16,10 +14,3 @@ class App extends Component {
     }
 }
 
-function mapStateToProps(state) {
-    return {
-        appState: state
-    }
-}
-
-export default connect(mapStateToProps)(App)
